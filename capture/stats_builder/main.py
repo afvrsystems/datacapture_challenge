930843a4dc7d9f6e3f1ec39ceb3323f4a2447ab3
afvrsystems <afvrsystems@gmail.com>
2023-11-28 00:46:41 -0500

    fix comments

--- capture/stats_builder/main.py
+++ capture/stats_builder/main.py
@@ -13,27 +13,27 @@ class StatsBuilder(AbstractStatsBuilder):
         self.max_value = 0
         self.total_value_qty = 0
 
-    def load_data(self, values: deque) -> AbstractStatsBuilder:  # dict().get() has O(1) complexity
-        self.clear()  # dict.clear() has O(1) complexity
-        self.max_value = max(values)  # max() has O(n) complexity
-        self.total_value_qty = len(values)  # len() has O(1) complexity
-        for num in range(1, self.max_value + 1):  # for-loop has O(n) complexity
-            self.less_than_index[num] = sum(  # dict.values() and sum() have O(n) complexity
+    def load_data(self, values: deque) -> AbstractStatsBuilder: # dict().get() has O(1) complexity
+        self.clear()                                    # dict.clear() has O(1) complexity
+        self.max_value = max(values)                    # max() has O(n) complexity
+        self.total_value_qty = len(values)              # len() has O(1) complexity
+        for num in range(1, self.max_value + 1):        # for-loop has O(n) complexity
+            self.less_than_index[num] = sum(            # dict.values() and sum() have O(n) complexity
                 self.histogram.values()
             )
-            self.histogram[num] = values.count(num)  # deque.count() has O(n) complexity
-            self.greater_than_index[num] = (  # dict.get() has O(1) complexity
+            self.histogram[num] = values.count(num)     # deque.count() has O(n) complexity
+            self.greater_than_index[num] = (            # dict.get() has O(1) complexity
                 self.total_value_qty - self.histogram[num] - self.less_than_index[num]
             )
         return self
 
-    def less(self, value: int) -> int:  # dict().get() has O(1) complexity
+    def less(self, value: int) -> int:                          # dict().get() has O(1) complexity
         return self.less_than_index[value] if value <= self.max_value else self.total_value_qty
 
-    def greater(self, value: int) -> int:  # dict().get() has O(1) complexity
+    def greater(self, value: int) -> int:                       # dict().get() has O(1) complexity
         return self.greater_than_index[value] if value <= self.max_value else 0
 
-    def between(self, start_val: int, end_val: int) -> int:  # dict().get() has O(1) complexity
+    def between(self, start_val: int, end_val: int) -> int:     # dict().get() has O(1) complexity
         return self.greater(start_val) - self.greater(end_val) + self.histogram.get(start_val, 0)
 
     def clear(self) -> None:
