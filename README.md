51df7ca95b3647709c40caef909714e771293416
afvrsystems <afvrsystems@gmail.com>
2023-11-28 11:57:54 -0500

    second approach, update readme

--- README.md
+++ README.md
@@ -43,3 +43,19 @@ print(stats.less(4))       # should return 2
 print(stats.between(3, 6)) # should return 4
 print(stats.greater(4))    # should return 2
 ```
+
+## Algorithm
+
+This package is time-efficient by hashing the captured data to increase the speed of stats operations. The method `add` of `DataCapture` and the methods `less`, `greater`, `between` of `StatsBuilder` have O(1) complexity. This means that no matter how many data is captured, it will take the same time to give a result. The method `build_stats` of `DataCapture` has O(n) complexity, whereby the execution time will increase with a linear behavior respect to the quantity of data.
+
+When analyzing the code it must be taken into account that there aren't O(n) nested operations. The most complex operation inside a O(n) operation must be O(1), and all the operations inside the method should be at most O(n).
+
+The code is based on the following:
+* The data is captured using a queue, which is memory-efficient and has O(1) append.
+* The hashes are stored in three dictionaries: `histogram`, `less_than_index`, and `greater_than_index`.
+* The `histogram` of frequencies has the quantity of appearances of each value in the captured data. First we build a dictionary with keys from 1 to the maximum value in the data, and all the values set to zero. Then it's populated by iterating the data and increasing by 1 respectively.
+* The indexes of **lesser values** and **greater values** are populated also by iterating from 1 to the maximum value in the data, but applying the following operations:
+    - The `less_than_index` is equal to the additive aggregation of histogram's values. This is possible as the dict-comprehension acts as a sorted iteration increasing by 1, so it keeps the O(n) complexity.
+    - The `less_than_index` is shifted by 1 key, so we must substract the `histogram` from it to get the real **less** index.
+    - The `greater_than_index` is calculated as the inversion of the `less_than_index`, so it requires the quantity of data minus the `histogram`, minus the `less_than_index`.
+* `less`, `greater`, and `between` are retrieved from those indexes, so they have O(1) complexity. The `between` method is calculated by substracting the `greater_than_index` part of both limits of the range to create an intersection, plus the `histogram` value of the upper limit because both the **less** and **greater** indexes are exclusive.
--- capture/stats_builder/main.py
+++ capture/stats_builder/main.py
@@ -13,27 +13,41 @@ class StatsBuilder(AbstractStatsBuilder):
         self.max_value = 0
         self.total_value_qty = 0
 
-    def load_data(self, values: deque) -> AbstractStatsBuilder: # dict().get() has O(1) complexity
-        self.clear()                                    # dict.clear() has O(1) complexity
-        self.max_value = max(values)                    # max() has O(n) complexity
-        self.total_value_qty = len(values)              # len() has O(1) complexity
-        for num in range(1, self.max_value + 1):        # for-loop has O(n) complexity
-            self.less_than_index[num] = sum(            # dict.values() and sum() have O(n) complexity
-                self.histogram.values()
-            )
-            self.histogram[num] = values.count(num)     # deque.count() has O(n) complexity
-            self.greater_than_index[num] = (            # dict.get() has O(1) complexity
+    def load_data(self, values: deque) -> AbstractStatsBuilder:  # dict().get() has O(1) complexity
+        self.clear()  # dict.clear() has O(1) complexity
+        max_value = max(values)  # max() has O(n) complexity
+        max_value_iter = range(1, max_value + 1)  # iterable building has O(1) complexity
+        self.total_value_qty = len(values)  # len() has O(1) complexity
+        self.histogram = {n: 0 for n in max_value_iter}  # dict comprehension has O(n) complexity
+
+        for val in values:  # for-loop has O(n) complexity
+            self.histogram[val] += 1
+
+        for num in max_value_iter:  # for-loop has O(n) complexity
+            self.less_than_index[num] = self.histogram[num] + self.less_than_index.get(
+                num - 1, 0
+            )  # dict.get() has O(1) complexity
+
+        self.less_than_index = {  # for-loop has O(n) complexity
+            num: self.histogram[num] + self.less_than_index.get(num - 1, 0)  # dict.get() has O(1) complexity
+            for num in max_value_iter
+        }
+
+        for num in max_value_iter:  # for-loop has O(n) complexity
+            self.less_than_index[num] -= self.histogram[num]  # dict.get() has O(1) complexity
+            self.greater_than_index[num] = (  # dict.get() has O(1) complexity
                 self.total_value_qty - self.histogram[num] - self.less_than_index[num]
             )
+
         return self
 
-    def less(self, value: int) -> int:                          # dict().get() has O(1) complexity
+    def less(self, value: int) -> int:  # dict().get() has O(1) complexity
         return self.less_than_index[value] if value <= self.max_value else self.total_value_qty
 
-    def greater(self, value: int) -> int:                       # dict().get() has O(1) complexity
+    def greater(self, value: int) -> int:  # dict().get() has O(1) complexity
         return self.greater_than_index[value] if value <= self.max_value else 0
 
-    def between(self, start_val: int, end_val: int) -> int:     # dict().get() has O(1) complexity
+    def between(self, start_val: int, end_val: int) -> int:  # dict().get() has O(1) complexity
         return self.greater(start_val) - self.greater(end_val) + self.histogram.get(start_val, 0)
 
     def clear(self) -> None:
